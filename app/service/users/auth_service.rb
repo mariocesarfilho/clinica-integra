70b67576dcb78a36d7bb6823bdc8851dea82b968
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-01-28 23:53:09 -0300

    Implement login method in AuthService for user authentication

--- app/service/users/auth_service.rb
+++ app/service/users/auth_service.rb
@@ -45,5 +45,25 @@ module Users
         token: token
       }
     end
+
+    def login
+      @user = search_user
+
+       @user.password_digest = gets.strip!
+
+      if BCrypt::Password.new(@user.password).is_password? @password_digest then
+        payload = { user_id: @user.id }
+
+        token = Rails.application.credentials.secret_key_base
+
+        JWT.decode(token)
+      else
+        throw Error("Usuário não encontrado!")
+      end
+    end
+
+    def search_user
+      User.find_by(email: @email)
+    end
   end
 end
