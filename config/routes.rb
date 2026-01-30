36f406985a6a937a890f2d224311617681d82463
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-01-29 22:16:11 -0300

    Remove create action from users resource in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [ :show, :update, :create, :destroy ]
+      resources :users, only: [ :show, :update, :destroy ]
       post "/register", to: "auths#create"
     end
   end
