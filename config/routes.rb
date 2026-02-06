092a4c3bb7bcb68d14f2ec0a0b677fde37b8332e
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-02-06 17:59:24 -0300

    feat: remove create action from users routes and adjust sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [ :show, :update, :destroy, :create ]
-      post "/login", to: "sessions#login"
+      resources :users, only: [ :show, :update, :destroy ]
+      resources :sessions, only: [ :create ]
     end
   end
 end
