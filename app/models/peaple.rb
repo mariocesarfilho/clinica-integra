17fb47e3d7cfef194d30775350205eae83892c3d
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-02-23 22:17:58 -0300

    Fix Peaple model: add associations, secure password, and validations

--- app/models/peaple.rb
+++ app/models/peaple.rb
@@ -1,2 +1,10 @@
 class Peaple < ApplicationRecord
+  has_one :therapist, dependent: :destroy
+  has_one :responsible, dependent: :destroy
+  has_one :patient, dependent: :destroy
+
+  has_secure_papassword
+
+  validates :email, presence: :true, uniqueness: true
+  validetes :cpf, presence: :true, uniqueness: true
 end
