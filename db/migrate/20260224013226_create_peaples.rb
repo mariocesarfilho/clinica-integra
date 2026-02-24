40315fb2486e5e210191312fad319e53d1d7f277
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-02-23 22:33:45 -0300

    Add migration to create Peaples table with fields for name, email, cpf, birth_date, phone, and password_digest

--- db/migrate/20260224013226_create_peaples.rb
+++ db/migrate/20260224013226_create_peaples.rb
@@ -4,9 +4,9 @@ class CreatePeaples < ActiveRecord::Migration[8.1]
       t.string :name
       t.string :email
       t.string :cpf
-      t.string :birth_date
-      t.string :date
+      t.date :birth_date
       t.string :phone
+      t.string :password_digest
 
       t.timestamps
     end
