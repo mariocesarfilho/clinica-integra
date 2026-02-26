262764c49b9f7eb0c595f408d73a9958f665b4ea
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-02-25 21:54:34 -0300

    Replace users resource with therapists in API routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [ :show, :update, :destroy ]
+      resources :therapists, only: [ :show, :update, :destroy ]
       resources :sessions, only: [ :create ]
     end
   end
